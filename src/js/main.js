4a22be46b6c7b63be401bc2a9f9ca2b9df877757
Artem <12artemondrozhzhyn@gmail.com>
2023-03-17 21:55:11 +0200

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import burgerMenu from "./modules/burgermenu";
 import slider from "./modules/slider";
 import forms from "./modules/forms";
+import tabs from "./modules/tabs";
 
 window.addEventListener("DOMContentLoaded", () => {
+  tabs(".header__menu", ".header__menu-link", "section", "animate__fadeIn");
   burgerMenu();
   slider("section", ".scroll");
   slider(".works__wrapper", ".works__media-btn");
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,54 @@
+import progressBar from "./progresBar";
+const tabs = (
+  headerSelector,
+  tabSelector,
+  contentSelector,
+  activeClass,
+  display = "flex"
+) => {
+  const header = document.querySelector(headerSelector),
+    tab = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSelector);
+
+  function hideTabContent() {
+    content.forEach((item) => {
+      item.style.display = "none";
+      item.classList.add("animate__animated");
+      item.classList.remove(activeClass);
+    });
+  }
+
+  function showTabContent(i = 0) {
+    content[i].style.display = display;
+    content[i].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (
+      target &&
+      (target.classList.contains(tabSelector.replace(/\./, "")) ||
+        target.parentNode.classList.contains(tabSelector.replace(/\./, "")))
+    ) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+        if (i == 3) {
+          progressBar("#HTML5", 95);
+          progressBar("#CSS3", 85);
+          progressBar("#JavaScript", 80);
+          progressBar("#ReactJS", 75);
+          progressBar("#Redux", 70);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
